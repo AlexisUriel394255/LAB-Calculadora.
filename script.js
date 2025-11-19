b28f2560c00015f068df29acbc9d6090f0efd2d3
Alexis Castillo <uclastle9112342@gmail.com>
2025-11-19 13:58:08 -0600

    se agrego las siguientes funciones de multiplicar,restar,dividir

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+//Primero cree una funcion llamada suma 
 function suma(a,b){
-  return a+b;
+  return a+b; /*aqui le pido que me haga la suma 
+entre a  y b para que despues me retorne el resultado
+*/
 }
-let resultado = suma(4, 4);
-console.log(resultado);
+let resultado = suma(4, 4); /*creo una variable llamada resultado
+y le asigno ala funcion suma dos numeros que me realizara la operacion en la 
+funcion
+*/ 
+console.log("La suma es:"+resultado);/*finalmente lo muestro en consola el 
+resultado de la funcion suma entre los dos numeros*/
+
+/*Ahora creo la funcion multiplicacion*/
+function multiplicacion(a,b){
+   return a*b /*aqui se hara la operacion que es la multiplicacion entre los 
+dos valores*/
+} 
+resultado = multiplicacion(5,5);/*Ahora ala variable le asingo la funcion con
+los numero que deseo que se haga la multiplicacion*/
+console.log("la multiplicacion es: "+resultado);/*Ahora muestro resultado*/
+/*Ahora creo la funcion de dividir*/
+function dividir(a,b){
+ return a / b; /*Aqui se hara la division de los dos valores adentro de la
+funcion*/
+}
+resultado = dividir(2,10);/*Le asigne los numeros que se diviran adentro de 
+la funcion que esta en resultado*/
+console.log("la division es:"+resultado);/*Ahora se muestra el resultado*/
+
+/*Por ultimo Ahora la funcion de restar la creo*/
+function resta(a,b){
+ return a-b; /*Ahora se hace la operacion de restar a menos b adentro de 
+la funcion*/
+}
+resultado = resta(20,5);/*le vuelvo asignar los numeros que se restaran ala 
+funcion adentro de la variable*/
+console.log("La resta es:"+resultado);
+
+
+
